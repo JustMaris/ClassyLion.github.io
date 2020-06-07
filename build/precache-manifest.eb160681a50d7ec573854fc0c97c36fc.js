ffabcff8de57d07cda3184f2b33e9360f9b30eed
Maris Popens <maris@popens.eu>
2020-06-07 14:00:04 +0300

    DeveloperFolio

---
+++ build/precache-manifest.eb160681a50d7ec573854fc0c97c36fc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1ceba9cdba429c4053b6cea4c9a407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0ae8aec650ba756459",
+    "url": "/static/css/main.012f654b.chunk.css"
+  },
+  {
+    "revision": "f7ce4b35112011d5bd39",
+    "url": "/static/js/2.f3b310e0.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.f3b310e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0ae8aec650ba756459",
+    "url": "/static/js/main.9f37e593.chunk.js"
+  },
+  {
+    "revision": "540b042b2fec6aced08c",
+    "url": "/static/js/runtime-main.95f9fd07.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
